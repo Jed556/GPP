573b8bb7b018b2fda157d8522addedc9b126fb05
Jed556 <jguiriba11@gmail.com>
2023-03-08 00:03:05 +0800

    feat: Add game engine

---
+++ engine.cpp
@@ -0,0 +1,45 @@
+#include "games.h"  // HEADER
+
+// INCLUDES
+#include <cctype>
+#include <iostream>
+#include <string>
+
+/**
+ * @brief Clear screen
+ */
+void clrscr() {
+    system("cls");
+}
+
+/**
+ * @brief Convert each value to lowercase
+ *
+ * @param input String
+ * @return Lowercase string
+ */
+std::string toLowerEach(std::string input) {
+    std::string res;
+    for (int i = 0; i < input.length(); i++)
+        res[i] = tolower(input[i]);
+
+    return res;
+}
+
+/**
+ * @brief Handles Prompts
+ *
+ * @param message Message to display for prompt
+ * @param lowercase Return input in lowercase
+ * @return Input string
+ */
+std::string stringPrompt(std::string message, bool lowercase) {
+    std::string answer;
+    std::cout << message;
+    std::cin >> answer;
+    if (lowercase)
+        for (char &c : answer)
+            c = std::tolower(c);
+
+    return answer;
+}
---
+++ games.h
@@ -0,0 +1,10 @@
+// INCLUDES
+#include <string>
+
+// ENGINE
+void clrscr();
+std::string toLowerEach(std::string input);
+std::string stringPrompt(std::string message, bool lowercase);
+
+// GAMES
+int blackjack();

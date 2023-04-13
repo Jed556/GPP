e6ff36aabe96fe74ea83443bec56dbdd3afbd314
Jed556 <jguiriba11@gmail.com>
2023-04-13 19:00:55 +0800

    feat: Update sample game

--- src/games/blackjack.cpp
+++ src/games/blackjack.cpp
@@ -13,12 +13,115 @@ int playerMoney = 1000, bet, prize;
 bool stand, hit;
 bool restart;
 
+// PROTOTYPES
+void title();
+bool exitPromptHandler(bool win, bool gameEnded);
+void DisplayCards(Hand dealer, Hand player, bool isStand);
+
+// GAME FUNCTIONS
+
+void gameLoop() {
+    // initialize game
+    Deck deck;
+
+    // initialize hands
+    Hand dealerHand(10), playerHand(10);
+    dealerHand.drawCards(deck);
+      std::cout << "done";
+    playerHand.drawCards(deck);
+
+    bool isGameOver = false;
+    bool isPlayerTurn = true;
+    bool isStand = false;
+
+    // player turn loop
+    while (!isGameOver && isPlayerTurn) {
+        DisplayCards(dealerHand, playerHand, isStand);
+
+        // player's turn
+        if (playerHand.getTotalValue()[0] > 21) {
+            std::cout << "Bust! You lost.\n\n";
+            isGameOver = true;
+            break;
+        }
+
+        hit = false;
+        stand = false;
+
+        while (!hit && !stand) {
+            answer = stringPrompt("Do you want to hit or stand? ", true);
+            if (answer == "hit") {
+                playerHand.drawCards(deck);
+                hit = true;
+            } else if (answer == "stand") {
+                isPlayerTurn = false;
+                isStand = true;
+                stand = true;
+            } else {
+                std::cout << "Invalid input. Please enter 'hit' or 'stand'.\n";
+            }
+        }
+    }
+
+    // dealer's turn
+    if (!isGameOver) {
+        isStand = true;
+        while (dealerHand.getTotalValue()[0] < 17) {
+            dealerHand.drawCards(deck);
+        }
+        DisplayCards(dealerHand, playerHand, isStand);
+
+        if (dealerHand.getTotalValue()[0] > 21) {
+            std::cout << "Dealer busts! You win!\n\n";
+            prize = bet * 2;
+            playerMoney += prize;
+        } else if (dealerHand.getTotalValue()[0] > playerHand.getTotalValue()[0]) {
+            std::cout << "Dealer wins!\n\n";
+        } else if (dealerHand.getTotalValue()[0] == playerHand.getTotalValue()[0]) {
+            std::cout << "It's a tie.\n\n";
+            playerMoney += bet;
+        } else {
+            std::cout << "You win!\n\n";
+            prize = bet * 2;
+            playerMoney += prize;
+        }
+
+        isGameOver = !exitPromptHandler((prize > 0), true);
+    }
+}
+
 /**
  * @brief Start Blackjack game
  *
  * @return Error status (int)
  */
 int blackjack() {
+    title();
+
+    while (true) {
+        std::cout << "You have $" << playerMoney << ".\n";
+        bet = intPrompt("How much do you want to bet? ");
+
+        if (bet > playerMoney) {
+            std::cout << "You don't have enough money to bet $" << bet << ".\n\n";
+            if (!exitPromptHandler(false, false))
+                break;
+        } else if (bet <= 0) {
+            std::cout << "Invalid bet. Please enter a positive number.\n\n";
+        } else {
+            playerMoney -= bet;
+            gameLoop();
+
+            if (playerMoney <= 0) {
+                std::cout << "You're out of money. Game over!\n";
+                break;
+            } else if (!restart) {
+                break;
+            }
+        }
+    }
+
+    std::cout << "Thanks for playing!\n";
     return 0;
 }
 

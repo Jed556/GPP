f0cbeaf87cf6ce06a25ff1746b3c9be69a1456bf
Jed556 <jguiriba11@gmail.com>
2023-04-13 15:09:18 +0800

    refactor: Update engine.h

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -1,8 +1,10 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include "cards/cards.h"
 #include "display/display.h"
 #include "prompt/prompt.h"
+#include "quiz/quiz.h"
 #include "text/text.h"
 
 #endif  // ENGINE_H

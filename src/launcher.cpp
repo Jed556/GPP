eae2153f71bf204f5f4160bb84151e030dcf9a76
Jed556 <jguiriba11@gmail.com>
2023-04-13 17:04:43 +0800

    feat: Add selections

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -1,20 +1,45 @@
+#include <iostream>
 #include <string>
 
-#include "games/games.h"
 #include "engine/engine.h"
+#include "games/games.h"
+
+Engine engine;
 
 int main() {
 Start:
-    std::string answer;
     std::string message;
+    std::string games[] = {"Blackjack (WIP)", "Multiple Choice", "Exit"};
 
     while (true) {
-        multiplechoice();
+        // Game selection
+        clrscr();
+        std::cout << "GPP (C++ Terminal Game Engine)\n"
+                  << "by Jed556 | version: " << engine.version() << "\n\n";
+
+        message = "Select index: ";
+        for (int i = 0; i < sizeof(games) / sizeof(games[0]); i++)
+            std::cout << "[" << i + 1 << "] " << games[i] << "\n";
+        int answer = intPrompt(message);
+
+        switch (answer) {
+            case 1:
+                blackjack();
+                break;
+            case 2:
+                multiplechoice();
+                break;
+            case 3:
+                return 0;
+            default:
+                message = "I didn't catch that.";
+                continue;
+        }
 
         while (true) {
             clrscr();
             message = "Do you like to restart launcher? ";
-            answer = stringPrompt(message, true);
+            std::string answer = stringPrompt(message, true);
 
             // Can be added to game engine (soon)
             const std::string arrTrue[] = {"yes", "y", "yeah", "yep"};        // Responses for true
